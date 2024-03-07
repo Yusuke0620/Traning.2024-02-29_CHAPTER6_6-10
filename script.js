b3741284fbe786ffbf845da62c6a6b09e36990bc
Yusuke0620 <boston19851229@gmail.com>
2024-03-07 10:37:32 +0900

    finished

---
+++ script.js
@@ -0,0 +1,17 @@
+const items = document.querySelectorAll('.img-item');
+
+for (let i = 0; i < items.length; i++ ) {
+
+    const keyframes = [
+        { opacity: 0, transform: 'rotateY(0deg) rotateX(50deg)' },
+        { opacity: 1, transform: 'rotateY(0deg) rotateX(0deg)' },
+    ];
+
+    const options = {
+        duration: 600,
+        delay: i * 300,
+        fill: 'forwards',
+    };
+    
+    items[i].animate(keyframes, options);    
+}
